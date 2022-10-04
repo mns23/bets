e20360bbeeb86f5922d5d1c50ca7d6e9ece20461
Matteo Cannella <mns23@hotmail.it>
2022-10-04 04:00:12 +0200

    fix: payoff/settlement logics + code cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,45 @@
+//! # Bets Pallet
+//!
+//! A simple Substrate pallet that allows each account to play both the role of better and bookmaker.
+//!
+//! ## Overview
+//!
+//! The module allows each user to create a match to bet on and to place bets in matches created by other users,
+//! through the following dispatchable functions: 
+//!
+//! * **create_match:** Passing as arguments the ID of the external match, and the odds,
+//! 	it creates a match on which to act as a bookmaker and let other users bet on this.
+//! * **place_bet:** Allows a user to bet on an open match. To do this, the user need to select the ID of the match
+//! 	on which bet on, the predicted result and the amount wagered. Once the transaction and the bet have been submitted,
+//! 	an amount equal to the bet one will be reserved in the bettor's account, an amount equal to the bet one multiplied
+//! 	by the established odds will be reserved in the bookmaker's account.
+//! * **set_match_result:** Retrieves the match result and saves it in storage. Subsequently, based on the latter,
+//! 	it scrolls all the bets related to that match and establishes the outcome, unreserving the entire amount of the bet
+//! 	to the winner (bettor or bookmaker). N.B.:
+//!     	* This call that can be made by any user at the moment, should be scheduled after the end of the event,
+//! 		saving the end-of-event timestamp among the match data.
+//!     	* The retrieval of a match result should be done through HTTP request using an ocw. To simplify this function,
+//! 		the RandomnessCollectiveFlip implementation of Randomness was used to generate the scores of the teams.
+
+
 #![cfg_attr(not(feature = "std"), no_std)]
 
-/// Edit this file to define custom logic or remove it if it is not needed.
-/// Learn more about FRAME and the core library of Substrate FRAME pallets:
-/// <https://docs.substrate.io/reference/frame-pallets/>
 use codec::{Decode, Encode};
 use frame_support::{
-	dispatch::{DispatchResult, Dispatchable, GetDispatchInfo},
+	dispatch::{DispatchResult},
 	ensure,
 	pallet_prelude::*,
-	storage::bounded_vec::BoundedVec,
-	traits::{Currency, ExistenceRequirement, Get, ReservableCurrency, WithdrawReasons, BalanceStatus, Randomness},
+	traits::{Currency, Get, ReservableCurrency, BalanceStatus, Randomness},
 	PalletId, RuntimeDebug,
 };
 pub use pallet::*;
 use frame_support::sp_runtime::{
-	traits::{AccountIdConversion, Saturating, Zero},
-	ArithmeticError, DispatchError,
+	traits::{Saturating, Zero},
 };
 use sp_std::prelude::*;
 //pub use weights::WeightInfo;
 
-pub type MatchIndex = u32;
+
 pub type BetIndex = u32;
 type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
 type BalanceOf<T> = <<T as Config>::Currency as Currency<AccountIdOf<T>>>::Balance;
@@ -29,6 +48,8 @@ type BetInfoOf<T> = Bet<AccountIdOf<T>, BalanceOf<T>>;
 #[derive(
 	Encode, Decode, Default, Clone, RuntimeDebug, MaxEncodedLen, TypeInfo, PartialEq, Copy,
 )]
+
+/// A Match have an initial state (Open), and 2 final states (Closed, Postponed).
 pub enum MatchStatus {
 	#[default]
 	Open,
@@ -43,7 +64,6 @@ pub struct SingleMatch<AccountId> {
 	pub owner: AccountId,
 	pub id_match: u32,
 	pub status: MatchStatus,
-	//pub description: String,
 	pub home_score: u32,
 	pub away_score: u32,
 	pub odd_homewin: u32,
@@ -85,83 +105,71 @@ pub struct Bet<AccountId, Balance> {
 	pub amount: Balance,
 }
 
-
 #[frame_support::pallet]
 pub mod pallet {
 	use super::*;
-	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
-
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
-	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
-		/// The bets pallet id
+		/// The bets pallet id.
 		#[pallet::constant]
 		type PalletId: Get<PalletId>;
-
-		/// Because this pallet emits events, it depends on the runtime's definition of an event.
+		/// Event type.
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+		/// The currency mechanism.
 		type Currency: ReservableCurrency<Self::AccountId>;
+		/// Something that provides randomness in the runtime.
 		type Randomness: Randomness<Self::Hash, Self::BlockNumber>;
 	}
 
-	// The pallet's runtime storage items.
-	// https://docs.substrate.io/main-docs/build/runtime-storage/
 	#[pallet::storage]
+	/// Mapping matches using its id as key.
 	#[pallet::getter(fn matches_by_id)]
 	pub(super) type Matches <T> =
 		StorageMap<_, Blake2_128Concat, u32, SingleMatch<AccountIdOf<T>>, OptionQuery>;
 	
 	#[pallet::storage]
+	/// A Storage Double Map of bets. Referenced by the id match to which it refers
+	/// (to quickly find all the bets related to a specific match), and its index.
 	#[pallet::getter(fn bets)]
 	pub(super) type Bets<T: Config> =
 		StorageDoubleMap<_, Blake2_128Concat, u32, Blake2_128Concat, BetIndex, BetInfoOf<T>, OptionQuery>;
 
 	#[pallet::storage]
+	/// Auto-incrementing bet counter
 	#[pallet::getter(fn bets_count)]
 	pub(super) type BetCount<T: Config> = StorageValue<_, BetIndex, ValueQuery>;
-	// Learn more about declaring storage items:
-	// https://docs.substrate.io/main-docs/build/runtime-storage/#declaring-storage-items
-	// pub type Something<T> = StorageValue<_, u32>;
 
-	// Pallets use events to inform users when important changes are made.
-	// https://docs.substrate.io/main-docs/build/events-errors/
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
+		/// A match was created.
 		MatchCreated(u32),
+		/// A bet was placed.
 		BetPlaced(BetIndex),
+		/// A match was closed.
 		MatchClosed(u32),
-		/// Event emitted when a claim has been created.
-		ClaimCreated { who: T::AccountId, claim: T::Hash },
-		/// Event emitted when a claim is revoked by the owner.
-		ClaimRevoked { who: T::AccountId, claim: T::Hash },
 	}
 
-	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
+		/// A specific match does not exist.
 		MatchNotExists,
+		/// No bet associated with a specific match exists.
 		NoBetExists,
-		ClaimFailed,
-		/// The claim already exists.
-		AlreadyClaimed,
-		/// The claim does not exist, so it cannot be revoked.
-		NoSuchClaim,
-		/// The claim is owned by another account, so caller can't revoke it.
-		NotClaimOwner,
+		/// Payoff procedure failed.
+		PayoffError,
 	}
 
-	// Dispatchable functions allow users to interact with the pallet and invoke state changes.
-	// These functions materialize as "extrinsics", which are often compared to transactions.
-	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
+		/// Passing as arguments the ID of the external match, and the odds,
+		/// it creates a match on which to act as a bookmaker and let other users bet on this.
 		#[pallet::weight(10_000)]
 		pub fn create_match(
 			origin: OriginFor<T>,
@@ -201,6 +209,10 @@ pub mod pallet {
 			Ok(())
 		}
 
+		/// Allows a user to bet on an open match. To do this, the user need to select the ID of the match
+		/// on which bet on, the predicted result and the amount wagered. Once the transaction and the bet have been submitted,
+		/// an amount equal to the bet one will be reserved in the bettor's account, an amount equal to the bet one multiplied
+		/// by the established odds will be reserved in the bookmaker's account.
 		#[pallet::weight(10_000)]
 		pub fn place_bet(
 			origin: OriginFor<T>,
@@ -209,14 +221,7 @@ pub mod pallet {
 			amount: BalanceOf<T>,
 		) -> DispatchResult {
 			let bet_owner = ensure_signed(origin)?;
-
-			//ensure!(end > now, <Error<T>>::EndTooEarly);
-
 			let index = BetCount::<T>::get();
-			// not protected against overflow, see safemath section
-			BetCount::<T>::put(index + 1);
-
-
 			let selected_match = Self::matches_by_id(id_match).ok_or(Error::<T>::MatchNotExists)?;
 			let match_owner = selected_match.owner;
 
@@ -235,7 +240,7 @@ pub mod pallet {
 			};
 
 			T::Currency::reserve(&bet_owner, amount)?;
-			//todo: add mod arithmetic
+			//todo: add mod arithmetic for fixed point odd.
 			T::Currency::reserve(&match_owner, amount.saturating_mul((odd as u32).into()))?;
 
 			let bet = Bet {
@@ -245,25 +250,34 @@ pub mod pallet {
 				odd,
 				amount,
 			};
-
+			// not protected against overflow, see safemath section.
+			BetCount::<T>::put(index + 1);
 			<Bets<T>>::insert(id_match, index,bet);
 
 			Self::deposit_event(Event::BetPlaced(index));
 			Ok(().into())
 		}
 
+		/// Retrieves the match result and saves it in storage. Subsequently, based on the latter,
+		/// it scrolls all the bets related to that match and establishes the outcome, unreserving the entire amount of the bet
+		/// to the winner (bettor or bookmaker). N.B.:
+		///     * This call that can be made by any user at the moment, should be scheduled after the end of the event,
+		/// 	saving the end-of-event timestamp among the match data.
+		///     * The retrieval of a match result should be done through HTTP request using an ocw. To simplify this function,
+		/// 	the RandomnessCollectiveFlip implementation of Randomness was used to generate the scores of the teams.
 		#[pallet::weight(10_000)]
 		pub fn set_match_result(
 			origin: OriginFor<T>,
 			id_match: u32,
 		) -> DispatchResult {
+			let _who = ensure_signed(origin)?;
 			let mut selected_match = Self::matches_by_id(id_match).ok_or(Error::<T>::MatchNotExists)?;
 			let match_owner = selected_match.owner.clone();
 			let selected_bets = <Bets<T>>::iter_prefix_values(id_match);
-			//Check if value null
+			// todo: check null value.
 
-			
-			//Update match status and results
+			// Update match status and results.
+			// todo: randomize also MatchStatus.
 			selected_match.status = MatchStatus::Closed;
 			selected_match.home_score = Self::generate_random_score(0);
 			selected_match.away_score = Self::generate_random_score(1);
@@ -273,7 +287,8 @@ pub mod pallet {
 			// 	Ok(())
 			// });
 			
-			//Check the winning status of the bet compared to match results
+			// Check the winning status of the bet compared to match results.
+			let mut payoff_result = true;
 			selected_bets.for_each(|bet|{
 				let bet_status: BetStatus = match &bet.prediction {
 					Prediction::Homewin if selected_match.home_score > selected_match.away_score => BetStatus::Won,
@@ -284,19 +299,27 @@ pub mod pallet {
 					_ => BetStatus::Lost,
 				};
 				
+				// Pay off the bet.
 				if bet_status == BetStatus::Won {
-					//maybe unwrap_or
-					T::Currency::repatriate_reserved(&match_owner, &(bet.owner), bet.amount.saturating_mul((bet.odd as u32).into()), BalanceStatus::Free).unwrap();
-					T::Currency::unreserve(&(bet.owner), bet.amount);
+					let repatriate_result_mtob = T::Currency::repatriate_reserved(&match_owner, &(bet.owner), bet.amount.saturating_mul((bet.odd as u32).into()), BalanceStatus::Free).is_ok();
+					let repatriate_result_btom = T::Currency::repatriate_reserved(&(bet.owner), &match_owner, bet.amount, BalanceStatus::Free).is_ok();
+					if repatriate_result_mtob == false || repatriate_result_btom == false {
+						payoff_result = false;
+					}
 				} else {
-					T::Currency::repatriate_reserved(&(bet.owner), &match_owner.clone(), bet.amount, BalanceStatus::Free).unwrap();
-					T::Currency::unreserve(&match_owner, bet.amount);
+					let repatriate_result = T::Currency::repatriate_reserved(&(bet.owner), &match_owner, bet.amount.clone(), BalanceStatus::Free).is_ok();
+					let unreserve_result = T::Currency::unreserve(&match_owner, bet.amount.saturating_mul((bet.odd as u32).into()));
+					if repatriate_result == false || !unreserve_result.is_zero() {
+						payoff_result = false;
+					}
 				}
-				//change bet status and save
+				
+				//change bet status and save.
 				//bet.status = new_bet_status;
 				//let key : u8 = selected_bets.last_raw_key();
 				//<Bets<T>>::insert(id_match, selected_bets.last_raw_key(),bet);
 			});
+			ensure!(payoff_result == true, Error::<T>::PayoffError);
 			Self::deposit_event(Event::MatchClosed(id_match));
 			Ok(().into())
 		}
@@ -305,7 +328,7 @@ pub mod pallet {
 }
 
 impl<T: Config> Pallet<T> {
-	///internal function from lottery pallet
+	///internal function from lottery pallet.
 	fn generate_random_score(seed_diff: u32) -> u32 {
 		let mut random_number = Self::generate_random_number(seed_diff);
 		let max_trials: u32 = 10;
@@ -322,7 +345,7 @@ impl<T: Config> Pallet<T> {
 		random_number % max_score
 	}
 
-	///internal function from lottery pallet
+	///internal function from lottery pallet.
 	fn generate_random_number(seed: u32) -> u32 {
 		let (random_seed, _) = T::Randomness::random(&(T::PalletId::get(), seed).encode());
 		let random_number = <u32>::decode(&mut random_seed.as_ref())
